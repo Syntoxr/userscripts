6eac8fc9e9b89e0526db02143fc115dac60b18a5
Syntox <63484271+Syntoxr@users.noreply.github.com>
2023-09-21 16:41:32 +0200

    Create timetable.user.js

---
+++ hs-os/timetable.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        hs-os timetable
+// @namespace   https://github.com/Syntoxr
+// @include     /^https:\/\/studienorganisation\.hs-osnabrueck\.de\/scripts\/mgrqispi\.dll\?APPNAME=CampusNet&PRGNAME=SCHEDULER/
+// @grant       GM_addStyle
+// @version     1.0
+// @author      Syntoxr
+// @description UI improvements like conflict highlighting and better readability
+// ==/UserScript==
+
+GM_addStyle ( `
+table td[abbr*="Spalte 1"] {
+  border-right: 1px solid black !important
+}
+` );
+
+let days = document.getElementsByClassName("weekday");
+
+//determine conflicting days and mark them in header
+for(let i = 0; i < 7; i++){
+  const day = days[i];
+  const colspan = day.getAttribute("colspan");
+
+  if (colspan > 1) {
+    day.style.cssText += 'background-color:red !important';
+  }
+}
