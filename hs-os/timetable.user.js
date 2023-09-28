38bc9185b289d926090080045f1ece6ec30f080e
Syntox <63484271+Syntoxr@users.noreply.github.com>
2023-09-28 12:14:07 +0200

    fix(hs-os-timetable): fixed day separation

--- hs-os/timetable.user.js
+++ hs-os/timetable.user.js
@@ -6,14 +6,14 @@
 // @homepageURL https://github.com/Syntoxr/userscripts/tree/main
 // @include     /^https:\/\/studienorganisation\.hs-osnabrueck\.de\/scripts\/mgrqispi\.dll\?APPNAME=CampusNet&PRGNAME=SCHEDULER/
 // @grant       GM_addStyle
-// @version     1.0
+// @version     1.1
 // @author      Syntoxr
 // @description UI improvements like conflict highlighting and better readability
 // ==/UserScript==
 
 GM_addStyle ( `
 table td[abbr*="Spalte 1"] {
-  border-right: 1px solid black !important
+  border-left: 1px solid black !important
 }
 ` );
 
